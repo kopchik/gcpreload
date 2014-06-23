223c58118174f0f9368a67628bb089225cd8c6df
Alexandre Kandalintsev <spam@messir.net>
2014-06-23 12:12:15 +0200

    test.c: error handling

--- test.c
+++ test.c
@@ -1,7 +1,13 @@
 #include <stdlib.h>
+#include <malloc.h>
+#include <assert.h>
 
 int main(void) {
-  while (1) {
-    void* ptr = malloc(1);
+  int i = 0;
+  mallopt(M_CHECK_ACTION, 3);
+  for (i=0; i<9000; i++) {
+    void* ptr = malloc(1024);
+	assert(ptr);
   }
+  return 0;
 }
